3420a94fcf08bffe59ce148d767e18171205a214
Sophia Behei <behey.sofiaa@gmail.com>
2026-03-20 22:05:09 +0200

    Revert "better structure 2"
    This reverts commit a595a515d203d085bf15392bc49beed476c03e2a.

--- Abstractions/Services/ICategoryService.cs
+++
@@ -1,8 +0,0 @@
-using NoticeBoard.DTO;
-
-namespace NoticeBoard.Services;
-
-public interface ICategoryService
-{
-    Task<List<CategoryOptionDto>> GetCategoryOptionsAsync();
-}
--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NoticeBoard.DTO;
 using NoticeBoard.Repositories;
 
 namespace NoticeBoard.Controllers;
@@ -21,6 +22,11 @@ public class UsersController(IUserRepository userRepository) : ControllerBase
             return BadRequest("Missing required claims.");
 
         var user = await userRepository.FindOrCreateAsync(sub, email, name);
-        return Ok(user);
+        return Ok(new CurrentUserDto
+        {
+            Id = user.Id,
+            Email = user.Email,
+            DisplayName = user.DisplayName
+        });
     }
 }
